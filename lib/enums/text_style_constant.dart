c8992a3f01e17a8edf67303c5108764f52601849
haikallf <haikallzrd@gmail.com>
2023-06-07 14:31:35 +0700

    add new color and text constants

---
+++ lib/enums/text_style_constant.dart
@@ -0,0 +1,185 @@
+import 'package:flutter/material.dart';
+
+class TextStyleConstant {
+  const TextStyleConstant();
+
+  TextStyle get heading01 => const TextStyle(
+        fontSize: 39,
+        decoration: TextDecoration.none,
+        fontStyle: FontStyle.normal,
+        fontWeight: FontWeight.w600,
+        height: 47 / 39,
+        letterSpacing: -0.32,
+      );
+
+  TextStyle get heading02 => const TextStyle(
+        fontSize: 31,
+        decoration: TextDecoration.none,
+        fontStyle: FontStyle.normal,
+        fontWeight: FontWeight.w600,
+        height: 37 / 31,
+        letterSpacing: -0.16,
+      );
+
+  TextStyle get heading03 => const TextStyle(
+        fontSize: 25,
+        decoration: TextDecoration.none,
+        fontStyle: FontStyle.normal,
+        fontWeight: FontWeight.w600,
+        height: 30 / 25,
+        letterSpacing: 0.07,
+      );
+
+  TextStyle get heading04 => const TextStyle(
+        fontSize: 20,
+        decoration: TextDecoration.none,
+        fontStyle: FontStyle.normal,
+        fontWeight: FontWeight.w600,
+        height: 24 / 20,
+        letterSpacing: 0.07,
+      );
+
+  TextStyle get heading05 => const TextStyle(
+        fontSize: 16,
+        decoration: TextDecoration.none,
+        fontStyle: FontStyle.normal,
+        fontWeight: FontWeight.w700,
+        height: 19 / 16,
+        letterSpacing: 0.15,
+      );
+
+  TextStyle get heading06 => const TextStyle(
+        fontSize: 13,
+        decoration: TextDecoration.none,
+        fontStyle: FontStyle.normal,
+        fontWeight: FontWeight.w700,
+        height: 16 / 13,
+        letterSpacing: 0.31,
+      );
+
+  TextStyle get title01 => const TextStyle(
+        fontSize: 25,
+        decoration: TextDecoration.none,
+        fontStyle: FontStyle.normal,
+        fontWeight: FontWeight.w400,
+        height: 30 / 25,
+        letterSpacing: 0,
+      );
+
+  TextStyle get title02 => const TextStyle(
+        fontSize: 20,
+        decoration: TextDecoration.none,
+        fontStyle: FontStyle.normal,
+        fontWeight: FontWeight.w400,
+        height: 24 / 20,
+        letterSpacing: 0,
+      );
+
+  TextStyle get title03 => const TextStyle(
+        fontSize: 16,
+        decoration: TextDecoration.none,
+        fontStyle: FontStyle.normal,
+        fontWeight: FontWeight.w500,
+        height: 19 / 16,
+        letterSpacing: 0,
+      );
+
+  TextStyle get paragraph01 => const TextStyle(
+        fontSize: 20,
+        decoration: TextDecoration.none,
+        fontStyle: FontStyle.normal,
+        fontWeight: FontWeight.w400,
+        height: 32 / 20,
+        letterSpacing: 0.47,
+      );
+
+  TextStyle get paragraph02 => const TextStyle(
+        fontSize: 16,
+        decoration: TextDecoration.none,
+        fontStyle: FontStyle.normal,
+        fontWeight: FontWeight.w400,
+        height: 26 / 16,
+        letterSpacing: 0.63,
+      );
+
+  TextStyle get paragraph03 => const TextStyle(
+        fontSize: 13,
+        decoration: TextDecoration.none,
+        fontStyle: FontStyle.normal,
+        fontWeight: FontWeight.w400,
+        height: 21 / 13,
+        letterSpacing: 0.8,
+      );
+
+  TextStyle get body01 => const TextStyle(
+        fontSize: 20,
+        decoration: TextDecoration.none,
+        fontStyle: FontStyle.normal,
+        fontWeight: FontWeight.w400,
+        height: 24 / 20,
+        letterSpacing: 0.63,
+      );
+
+  TextStyle get body02 => const TextStyle(
+        fontSize: 16,
+        decoration: TextDecoration.none,
+        fontStyle: FontStyle.normal,
+        fontWeight: FontWeight.w400,
+        height: 19 / 16,
+        letterSpacing: 0.63,
+      );
+
+  TextStyle get body03 => const TextStyle(
+        fontSize: 13,
+        decoration: TextDecoration.none,
+        fontStyle: FontStyle.normal,
+        fontWeight: FontWeight.w400,
+        height: 16 / 13,
+        letterSpacing: 0.63,
+      );
+
+  TextStyle get label01 => const TextStyle(
+        fontSize: 20,
+        decoration: TextDecoration.none,
+        fontStyle: FontStyle.normal,
+        fontWeight: FontWeight.w500,
+        height: 24 / 20,
+        letterSpacing: 0.15,
+      );
+
+  TextStyle get label02 => const TextStyle(
+        fontSize: 16,
+        decoration: TextDecoration.none,
+        fontStyle: FontStyle.normal,
+        fontWeight: FontWeight.w500,
+        height: 19 / 16,
+        letterSpacing: 0.31,
+      );
+
+  TextStyle get label03 => const TextStyle(
+        fontSize: 13,
+        decoration: TextDecoration.none,
+        fontStyle: FontStyle.normal,
+        fontWeight: FontWeight.w600,
+        height: 16 / 13,
+        letterSpacing: 0.31,
+      );
+
+  TextStyle get caption01 => const TextStyle(
+        fontSize: 10,
+        decoration: TextDecoration.none,
+        fontStyle: FontStyle.normal,
+        fontWeight: FontWeight.w500,
+        height: 12 / 10,
+        letterSpacing: 0.8,
+      );
+
+  TextStyle get caption02 => const TextStyle(
+        fontSize: 10,
+        decoration: TextDecoration.none,
+        fontStyle: FontStyle.normal,
+        fontWeight: FontWeight.w500,
+        height: 12 / 10,
+        letterSpacing: 1.12,
+      );
+}
